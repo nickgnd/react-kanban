ea51aaf441947b333edbc1ce1df87b827cba149e
nickgnd <nick.gnd@gmail.com>
2016-01-13 00:27:56 +0100

    Use AltContainer wrapper to simplify connection logic

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,3 +1,5 @@
+import AltContainer from 'alt-container';
+
 import React from 'react';
 import Notes from './Notes.jsx';
 
@@ -5,43 +7,27 @@ import NoteActions from '../actions/NoteActions';
 import NoteStore from '../stores/NoteStore';
 
 export default class App extends React.Component {
-  // Set props and state
-  constructor(props) {
-    super(props);
-
-    this.state = NoteStore.getState();
-  }
-
-  // React lifecycles hooks
-  componentDidMount() {
-    console.log("componentDidMount");
-    NoteStore.listen(this.storeChanged);
-  }
-  componentWillUnmount() {
-    console.log("componentWillUnmoun");
-    NoteStore.unlisten(this.storeChanged);
-  }
-
-  // Class property
-  storeChanged = (state) => {
-    // Without a property initializer `this` wouldn't
-    // point at the right context (defaults to `undefined` in strict mode).
-    this.setState(state);
-  };
 
   render() {
-
-    const notes = this.state.notes;
 
     return (
       <div>
 
         <button className="add-note" onClick={this.addNote}>+</button>
 
-        <Notes notes={notes}
+        {/* AltContainer allows us to bind data to its immediate children.
+        In this case, it injects the notes property in to Notes. */}
+        <AltContainer
+          stores={[NoteStore]}
+          inject={{
+            notes: () => NoteStore.getState().notes
+          }} >
+
+        <Notes
           onEdit={this.editNote}
           onDelete={this.deleteNote} />
 
+        </AltContainer>
       </div>
       );
   }
