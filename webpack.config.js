d82ec6dbade40a0fec64240b2d61a62180a06e94
nickgnd <nick.gnd@gmail.com>
2016-01-15 00:38:38 +0100

    Optimizing build size for production and set deploy script to github site

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,9 @@ const TARGET = process.env.npm_lifecycle_event;
 
 const webpack = require('webpack');
 
+// Load *package.json* so we can use `dependencies` from there
+const pkg = require('./package.json');
+
 
 const PATHS = {
   app: path.join(__dirname, 'app'),
@@ -30,17 +33,11 @@ const common = {
   },
   output: {
     path: PATHS.build,
-    filename: 'bundle.js'
+    // Output using entry name
+    filename: '[name].js'
   },
   module: {
     loaders: [
-      {
-        // Test expects a RegExp! Note the slashes!
-        test: /\.css$/,
-        loaders: ['style', 'css'],
-        // Include accepts either a path or an array of paths.
-        include: PATHS.app
-      },
 
       // Set up jsx. This accepts js too thanks to RegExp
       {
@@ -66,6 +63,10 @@ const common = {
 };
 
 
+const Clean = require('clean-webpack-plugin');
+
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 // Default configuration
 if(TARGET === 'start' || !TARGET) {
   module.exports = merge(common, {
@@ -85,12 +86,66 @@ if(TARGET === 'start' || !TARGET) {
       host: process.env.HOST,
       port: process.env.PORT
     },
+    module: {
+      loaders: [
+        // Define development specific CSS setup
+        {
+          test: /\.css$/,
+          loaders: ['style', 'css'],
+          include: PATHS.app
+        }
+      ]
+    },
     plugins: [
       new webpack.HotModuleReplacementPlugin()
     ]
   });
 }
 
-if(TARGET === 'build') {
-  module.exports = merge(common, {});
+if(TARGET === 'build' || TARGET === 'stats') {
+  module.exports = merge(common, {
+    // Define entry points needed for splitting
+    entry: {
+      app: PATHS.app,
+      vendor: Object.keys(pkg.dependencies).filter(function(v) {
+        // Exclude alt-utils as it won't work with this setup
+        // due to the way the package has been designed
+        // (no package.json main).
+        return v !== 'alt-utils';
+      })
+    },
+    output: {
+      path: PATHS.build,
+      filename: '[name].[chunkhash].js',
+      chunkFilename: '[chunkhash].js'
+    },
+    module: {
+      loaders: [
+        // Extract CSS during build
+        {
+          test: /\.css$/,
+          loader: ExtractTextPlugin.extract('style', 'css'),
+          include: PATHS.app
+        }
+      ]
+    },
+    plugins: [
+      new Clean([PATHS.build]),
+      // Output extracted CSS to a file
+      new ExtractTextPlugin('styles.[chunkhash].css'),
+      // Extract vendor and manifest files
+      new webpack.optimize.CommonsChunkPlugin({
+        names: ['vendor', 'manifest']
+      }),
+      // Setting DefinePlugin affects React library size!
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify('production')
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false
+        }
+      })
+    ]
+  });
 }
